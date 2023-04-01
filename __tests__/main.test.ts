85dd8aed8f5ade1dacb8e09efaa82b3c53cd8291
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2023-04-01 13:27:42 -0400

    Wrap all integration testing logic in `::stop-commands::`
    Signed-off-by: Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -4,13 +4,6 @@ import * as path from 'path'
 import {beforeEach, afterAll, describe, expect, test} from '@jest/globals'
 import {run_args} from '../src/main'
 
-test('test runs_args', async () => {
-  const values = await run_args('java', '44321, 33221 3111', '', '')
-  expect(values).toContain(44321)
-  expect(values).toContain(33221)
-  expect(values).toContain(3111)
-})
-
 describe('integration testing', () => {
   beforeEach(() => {
     // eslint-disable-next-line no-console
@@ -20,8 +13,15 @@ describe('integration testing', () => {
     // eslint-disable-next-line no-console
     console.log('::stoptoken::')
   })
+  test('test runs_args', async () => {
+    const values = await run_args('java', '44321, 33221 3111', '', '')
+    expect(values).toContain(44321)
+    expect(values).toContain(33221)
+    expect(values).toContain(3111)
+  })
+
   // shows how the runner will run a javascript action with env / stdout protocol
-  test('test runs', () => {
+  test('test run as spawned process', () => {
     const nodePath = process.execPath
     const ip = path.join(__dirname, '..', 'lib', 'main.js')
     // eslint-disable-next-line no-console
