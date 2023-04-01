aa349950c42d2a687b09227a8c8a24588809fa61
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2023-04-01 13:12:28 -0400

    Run environment tests with stoptoken
    Signed-off-by: Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,7 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import {beforeEach, afterAll, describe, expect, test} from '@jest/globals'
 import {run_args} from '../src/main'
 
 test('test runs_args', async () => {
@@ -11,19 +11,29 @@ test('test runs_args', async () => {
   expect(values).toContain(3111)
 })
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  const nodePath = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  // eslint-disable-next-line no-console
-  console.log(`Environment Before Merge: ${JSON.stringify(process.env)}`)
-  const options: cp.ExecFileSyncOptions = {
-    env: {...process.env, INPUT_PRODUCT: 'java', RUNNER_DEBUG: '1'}
-  }
-  // eslint-disable-next-line no-console
-  console.log(`Environment After Merge: ${JSON.stringify(options.env)}`)
-  const output = cp.execFileSync(nodePath, [ip], options).toString()
-  // eslint-disable-next-line no-console
-  console.log(output)
-  expect(output).toContain('::set-output name=versions::[')
+describe('integration testing', () => {
+  beforeEach(() => {
+    // eslint-disable-next-line no-console
+    console.log('::stop-commands::stoptoken')
+  })
+  afterAll(() => {
+    // eslint-disable-next-line no-console
+    console.log('::stoptoken::')
+  })
+  // shows how the runner will run a javascript action with env / stdout protocol
+  test('test runs', () => {
+    const nodePath = process.execPath
+    const ip = path.join(__dirname, '..', 'lib', 'main.js')
+    // eslint-disable-next-line no-console
+    console.log(`Environment Before Merge: ${JSON.stringify(process.env)}`)
+    const options: cp.ExecFileSyncOptions = {
+      env: {...process.env, INPUT_PRODUCT: 'java', RUNNER_DEBUG: '1'}
+    }
+    // eslint-disable-next-line no-console
+    console.log(`Environment After Merge: ${JSON.stringify(options.env)}`)
+    const output = cp.execFileSync(nodePath, [ip], options).toString()
+    // eslint-disable-next-line no-console
+    console.log(output)
+    expect(output).toContain('::set-output name=versions::[')
+  })
 })
